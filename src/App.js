f292064c030b7ca18e1e54c06d6630cca25b92e8
David Rhoderick <david@artlyticalmedia.com>
2021-05-26 21:48:14 +0200

    Styling table and adding custom named factors and decisions.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from 'react'
 import {css, Global} from '@emotion/react'
 import styled from '@emotion/styled'
 
@@ -12,6 +13,9 @@ const App = () => {
   const {list: decisions} = useSelector(state => state.decisions)
   const {list: factors} = useSelector(state => state.factors)
   const {matrix: decisionsFactors} = useSelector(state => state.decisionsFactors)
+
+  const [newDecision, setNewDecision] = useState('')
+  const [newFactor, setNewFactor] = useState('')
 
   return <div>
     <Global styles={css`
@@ -30,44 +34,91 @@ const App = () => {
       }
     `} />
 
-    <h1>Decision Matrix</h1>
+    <Title>Decision Matrix</Title>
 
-    <StyledTable>
-      <thead>
-        <tr>
-          <th>Factors</th>
-          {decisions.map((decision, index) => <th key={index}>{decision} {decisions.length > 1 && <Remove onClick={() => dispatch(removeDecision(index))} />}</th>)}
-          <th onClick={() => dispatch(addDecision('new decision'))}>Add decision</th>
-        </tr>
-      </thead>
+    <div>
+      <StyledTable>
+        <thead>
+          <tr>
+            <th>Factors</th>
+            {decisions.map((decision, index) => <th key={index}>{decision} {decisions.length > 1 && <Remove onClick={() => dispatch(removeDecision(index))} />}</th>)}
+            <th><StyledInput value={newDecision} onChange={(event) => setNewDecision(event.target.value)} placeholder="Add decision" /><Add onClick={() => {
+              if(newDecision.length > 0) {
+                dispatch(addDecision(newDecision))
+                setNewDecision('')
+              }
+            }}/></th>
+          </tr>
+        </thead>
 
-      <tbody>
-        {factors.map((factor, factorIndex) => <tr key={factorIndex}>
-          <td>{factor} {factors.length > 1 && <Remove onClick={() => dispatch(removeFactor(factorIndex))} />}</td>
-          {decisionsFactors[factorIndex].map((decisionFactor, decisionIndex) => <td key={decisionIndex} onClick={() => dispatch(incrementFactor({factor: factorIndex, decision: decisionIndex}))}>{decisionFactor}</td>)}
-        </tr>)}
-        <tr>
-          <td onClick={() => dispatch(addFactor('new factor'))}>Add factor</td>
-        </tr>
-      </tbody>
-    </StyledTable>
-
-    <StyledTable>
-      <tbody>
-        {decisions.map((decision, index) => <tr key={index}>
-          <th>{decision}</th>
-          <td>{decisionsFactors.reduce((sum, factor) => sum += factor[index], 0)}</td>
-        </tr>)}
-      </tbody>
-    </StyledTable>
+        <tbody>
+          {factors.map((factor, factorIndex) => <tr key={factorIndex}>
+            <td>{factor} {factors.length > 1 && <Remove onClick={() => dispatch(removeFactor(factorIndex))} />}</td>
+            {decisionsFactors[factorIndex].map((decisionFactor, decisionIndex) => <td key={decisionIndex} onClick={() => dispatch(incrementFactor({factor: factorIndex, decision: decisionIndex}))}>{decisionFactor}</td>)}
+          </tr>)}
+          <tr>
+            <td><StyledInput value={newFactor} onChange={(event) => setNewFactor(event.target.value)} placeholder="Add factor" /><Add onClick={() => {
+              if(newFactor.length > 0) {
+                dispatch(addFactor(newFactor))
+                setNewFactor('')
+              }
+            }}/></td>
+          </tr>
+        </tbody>
+      </StyledTable>
+    </div>
+    
+    <div>
+      <StyledTable>
+        <tbody>
+          {decisions.map((decision, index) => <tr key={index}>
+            <th>{decision}</th>
+            <td>{decisionsFactors.reduce((sum, factor) => sum += factor[index], 0)}</td>
+          </tr>)}
+        </tbody>
+      </StyledTable>
+    </div>
   </div>
 }
 
 export default App
 
+const Title = styled.h1`
+  margin: 3rem 2rem 1rem;
+`
+
 const StyledTable = styled.table`
+  margin: 1rem 2rem;
+  border-collapse: collapse;
+
   th, td {
-    padding: 0.5rem;
+    padding: 1rem;
+
+    text-align: center;
+
+    border: 1px solid #222;
+
+    &:first-of-type {
+      text-align: left;
+    }
+  }
+
+  td:not(:first-of-type) {
+    cursor: pointer;
+  }
+`
+
+const Add = styled.button`
+  outline: none;
+  background: none;
+  border: none;
+  cursor: pointer;
+  font-size: 2rem;
+  vertical-align: sub;
+
+  :after {
+    content: '\\002B';
+    display: inline-block;
   }
 `
 
@@ -76,9 +127,18 @@ const Remove = styled.button`
   background: none;
   border: none;
   cursor: pointer;
+  font-size: 2rem;
+  vertical-align: sub;
 
   :after {
     content: '\\00D7';
     display: inline-block;
   }
+`
+
+const StyledInput = styled.input`
+  outline: none;
+  border: none;
+  padding-bottom: 0.375rem;
+  border-bottom: 1px solid #222;
 `
