e50f51539e5479ca73cc2996d6be3486abc53a09
David Rhoderick <david@artlyticalmedia.com>
2021-05-28 21:08:11 +0200

    Fixing #8.

--- src/store.js
+++ src/store.js
@@ -1,13 +1,22 @@
 import {configureStore} from '@reduxjs/toolkit'
 
+import {loadState, saveState} from './localStorage'
+
 import choices from './choicesSlice'
 import factors from './factorsSlice'
 import factorsChoices from './factorsChoicesSlice'
 
-export default configureStore({
+const store = configureStore({
   reducer: {
     choices,
     factors,
     factorsChoices
-  }
+  },
+  preloadedState: loadState()
 })
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
+
+export default store
