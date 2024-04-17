06bb3d0768712acfacb3cf691b78c2f2521c38d6
David Rhoderick <david@artlyticalmedia.com>
2024-04-17 11:02:43 +0200

    Adding authenticate thunk for use with RTK Query

--- src/redux/authSlice.ts
+++ src/redux/authSlice.ts
@@ -1,14 +1,55 @@
-import { PayloadAction, createSlice } from "@reduxjs/toolkit";
+import { PayloadAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+
+export type AuthResponse = {
+  tokenType: string;
+  session: {
+    id: string;
+    userId: string;
+    fresh: boolean;
+    expiresAt: string;
+  };
+};
+
+type RequestState = "uninitialized" | "pending" | "fulfilled" | "rejected";
 
 export interface AuthState {
   tokenType: string;
   accessToken: string;
+  state: RequestState;
 }
-
 const initialState: AuthState = {
-  tokenType: '',
-  accessToken: ''
+  tokenType: "",
+  accessToken: "",
+  state: "uninitialized",
 };
+
+export const authenticate = createAsyncThunk<
+  AuthResponse,
+  { username: string; password: string }
+>("auth/login", async ({ username, password }, { rejectWithValue }) => {
+  try {
+    const response = await fetch(`${import.meta.env.VITE_BACKEND_URL}/login`, {
+      method: "POST",
+      headers: {
+        "content-type": "application/json",
+      },
+      body: JSON.stringify({ username, password }),
+    });
+    return response.json();
+  } catch (error) {
+    rejectWithValue(error);
+    // if (error.response) {
+    //   return rejectWithValue({
+    //     status: error.response?.status ? error.response.status : 500,
+    //     data: error.response?.data
+    //       ? { ...error.response.data }
+    //       : { message: "No error data provided" },
+    //   });
+    // } else {
+    //   return rejectWithValue(error.toString());
+    // }
+  }
+});
 
 export const choicesSlice = createSlice({
   name: "choices",
@@ -16,6 +57,22 @@ export const choicesSlice = createSlice({
   reducers: {
     setAuth: (_state, action: PayloadAction<AuthState>) => action.payload,
     clearAuth: () => initialState,
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(authenticate.pending, (state) => {
+        state.state = "pending";
+      })
+      .addCase(authenticate.fulfilled, (state, action) => {
+        state.accessToken = action.payload.session.id;
+        state.tokenType = action.payload.tokenType;
+        state.state = "fulfilled";
+      })
+      .addCase(authenticate.rejected, (state) => {
+        state.accessToken = "";
+        state.tokenType = "";
+        state.state = "rejected";
+      });
   },
 });
 
