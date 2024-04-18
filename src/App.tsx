25b4fd0046c7711ea98ad4a7a6c2e82ed30e960e
David Rhoderick <david@artlyticalmedia.com>
2024-04-18 11:36:16 +0200

    Installing MUI Joy and configuring

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,5 @@
-import { css, Global } from "@emotion/react";
 import { Provider } from "react-redux";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
-
-import colors from "@/lib/colors";
 
 import store from "@/redux/store";
 
@@ -11,6 +8,7 @@ import Login from "./components/Login";
 import Signup from "./components/Signup";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import { CssBaseline } from "@mui/joy";
 
 // eslint-disable-next-line prefer-const
 let persistor = persistStore(store);
@@ -33,24 +31,7 @@ const router = createBrowserRouter([
 const App = () => (
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
-      <Global
-        styles={css`
-          body {
-            margin: 0;
-            font-family: -apple-system, BlinkMacSystemFont, "Segoe UI", "Roboto",
-              "Oxygen", "Ubuntu", "Cantarell", "Fira Sans", "Droid Sans",
-              "Helvetica Neue", sans-serif;
-            -webkit-font-smoothing: antialiased;
-            -moz-osx-font-smoothing: grayscale;
-            color: ${colors.body};
-          }
-
-          code {
-            font-family: source-code-pro, Menlo, Monaco, Consolas, "Courier New",
-              monospace;
-          }
-        `}
-      />
+      <CssBaseline />
 
       <RouterProvider router={router} />
     </PersistGate>
