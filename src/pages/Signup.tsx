e9166c1961a9130c8a93f4f36bc8a067469cc6fd
David Rhoderick <david@artlyticalmedia.com>
2024-04-22 12:54:42 +0200

    Fixing sonarlint error

--- src/pages/Signup.tsx
+++ src/pages/Signup.tsx
@@ -105,7 +105,7 @@ const Signup: FC = () => {
         rules={{
           pattern: {
             value:
-              /^[a-z0-9~!$%^&*_=+\-}{'?]+(\.[a-z0-9~!$%^&*_=+\-}{'?]+)*@([a-z0-9_][a-z0-9_]*(\.[a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|([0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}))(:[0-9]{1,5})?$/,
+              /^[a-z0-9~!$%^&*_=+\-}{'?]+(\.[a-z0-9~!$%^&*_=+\-}{'?]+)*@([a-z0-9_][a-z0-9_]*(\.[a-z0-9_]+)*\.(aero|arpa|biz|com|coop|edu|gov|info|int|mil|museum|name|net|org|pro|travel|mobi|[a-z][a-z])|(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}))(:\d{1,5})?$/,
             flags: "i",
             message: emailMessage,
           },
